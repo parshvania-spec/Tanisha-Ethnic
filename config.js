3622f6927d5e7632f3a70b05454ef87cee97cd0e
parshvania-spec <parshvania@gmail.com>
2026-08-15 19:53:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
-const SUPABASE_URL = 'https://cxydymcjgxgcdqgltnii.supabase.co';
+const SUPABASE_URL = 'https://yeoccpkjhpgtmfsrabxy.supabase.co';
 const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_dAnb4fSaI_BI_UZ9gXX9aA_i0zxXqj3';
 const SUPABASE_PROJECT_ID = 'cxydymcjgxgcdqgltnii';
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
