88543d5d198e588996dff9ded4eb2fb9053b7a97
parshvania-spec <parshvania@gmail.com>
2026-08-22 09:42:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-'use strict';
-const SUPABASE_URL = 'https://cxydymcjgxgcdqgltnii.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_dAnb4fSaI_BI_UZ9gXX9aA_i0zxXqj3';
-const SUPABASE_PROJECT_ID = 'cxydymcjgxgcdqgltnii';
-const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
-  auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true }
-});
+const SUPABASE_URL = 'https://yeoccpkjhpgtmfsrabxy.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_rlhJUBPorEKqKLUgClj30Q_7EhBXUEd';
+const SUPABASE_PROJECT_ID = 'yeoccpkjhpgtmfsrabxy';
+
